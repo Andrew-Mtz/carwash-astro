ad7dc6c5909f8e1ee3ea31e49353b4e7f11a27e9
Andrew-Mtz <andrew.nahuelMH@gmail.com>
2025-02-04 22:04:00 -0300

    fix: se arreglan links de metadatos

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://carflash.vercel.app",
+  site: "https://carwash-astro.vercel.app/",
   integrations: [sitemap()],
   build: {
     minify: true,
